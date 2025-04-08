4ab0a90e7c30c9211ebe4a12be8959f5ca90714b
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-04-08 22:00:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+let mybtn = document.getElementById("btn");
+
+mybtn.addEventListener("click",()=>{
+	let message = document.getElementById("text").value;
+    let delay = document.getElementById("delay").value;
+    delay = Number(delay);
+    let div = document.getElementById("output");
+
+    const p = new Promise((resolve)=>{
+	     setTimeout(()=>{
+		resolve(message);
+	    },delay);
+    });
+
+     async function f(){
+	const v = await p;
+	div.textContent = v;	
+     }
+
+    f();
+	
+});
+
